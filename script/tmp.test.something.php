9e9ce569917cd6ef3a2dccaf25795a1cc1c410d3
Lipeng_wei <lipeng_wei>
2020-08-17 12:29:33 +0800

    test something

--- script/tmp.test.something.php
+++ script/tmp.test.something.php
@@ -14,15 +14,50 @@ if ($argc > 0 && basename($argv[0]) == 'tmp.test.something.php') {
     }
     Log::info(basename($argv[0]) . ' 开始运行');
 
-    $arr = [
-        'ab' => 123,
-        'cd' => 234,
-        'ef' => 345,
-        'gh' => 456,
-        'ij' => 567,
-        'kk' => 678
-    ];
-    var_dump(array_slice($arr,1,2));
+//    $arr = [
+//        'ab' => 123,
+//        'cd' => 234,
+//        'ef' => 345,
+//        'gh' => 456,
+//        'ij' => 567,
+//        'kk' => 678
+//    ];
+//    var_dump(array_slice($arr,1,2));
+
+
+//    $file = dirname(__FILE__). '/../tmp/tdxline.dat';
+//    echo $file;
+//    $hexstr = file_get_contents($file);
+//    echo $hexstr;
+//    $data = pack('H*', $hexstr);
+//    echo $data;
+
 
     Log::info(basename($argv[0]) . ' 运行结束');
+}
+
+
+/**
+ * 将文件内容转为16进制输出
+ * @param String $file 文件路径
+ * @return String
+ */
+function fileToHex($file){
+    if(file_exists($file)){
+        $data = file_get_contents($file);
+        return bin2hex($data);
+    }
+    return '';
+}
+
+/**
+ * 将16进制内容转为文件
+ * @param String $hexstr 16进制内容
+ * @param String $file 保存的文件路径
+ */
+function hexToFile($hexstr, $file){
+    if($hexstr){
+        $data = pack('H*', $hexstr);
+        file_put_contents($file, $data, true);
+    }
 }
